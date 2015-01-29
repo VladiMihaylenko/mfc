008da29ba6bf2448a95c1dc8f32a78f14eee8c59
VladiMihaylenko <vxmihaylenko@gmail.com>
2015-01-29 03:03:29 +0300

    Add atomic mfc

---
+++ atomic_mfc.h
@@ -0,0 +1,15 @@
+//
+//  mfc.h
+//  mfc
+//
+//  Created by Vlad Mihaylenko on 27/01/15.
+//  Copyright (c) 2015 Vlad Mihaylenko. All rights reserved.
+//
+
+#ifndef __mfc__mfc__
+#define __mfc__mfc__
+
+char mostFrequentCharacter(char* str, int size);
+
+
+#endif /* defined(__mfc__mfc__) */
